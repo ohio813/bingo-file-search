b4fc764a3997512ecba63ee4169da10a37aec235
michael911009 <michael911009@gmail.com>
2011-12-21 08:45:02 +0000

    add trylock in Mutex

--- core-test/core/ThreadSync.h
+++ core-test/core/ThreadSync.h
@@ -1,18 +1,18 @@
 /**
-*	Copyright (C) 2011  Xu Cheng, Yang Zhengyu ,Zuo Zhiheng
+*   Copyright (C) 2011  Xu Cheng, Yang Zhengyu ,Zuo Zhiheng
 *
-*	This program is free software: you can redistribute it and/or modify
-*	it under the terms of the GNU General Public License as published by
-*	the Free Software Foundation, either version 3 of the License, or
-*	(at your option) any later version.
-*	
-*	This program is distributed in the hope that it will be useful,
-*	but WITHOUT ANY WARRANTY; without even the implied warranty of
-*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*	GNU General Public License for more details.
-*	
-*	You should have received a copy of the GNU General Public License
-*	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 ///:ThreadSync.h
 #ifndef THREADSYNC_H
@@ -26,6 +26,7 @@ public:
     Mutex();
     ~Mutex();
     void lock();
+    bool trylock();
     void unlock();
 private:
     CRITICAL_SECTION m_criticalSection;
